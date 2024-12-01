bba52078b696db27afb505eca35c75b7614a2149
NateDCoder <rubyandtwixie@gmail.com>
2024-11-30 21:56:36 -0500

    Events tabs

--- Javscript Files/events.js
+++ Javscript Files/events.js
@@ -1,3 +1,30 @@
+async function generateEvents() {
+    const eventNames = await fetchEventNames();
+    const eventContainer = document.getElementsByClassName("events-container")[0];
+    Object.entries(eventNames).forEach(([code, name]) => {
+        const link = document.createElement("a")
+        link.href = "../HTML files/events htmls/"+code+".html"
+        const div = document.createElement("div");
+        div.className = "event-card";
+        const h3 = document.createElement("h3");
+        h3.innerText = name
+        div.append(h3)
+        link.append(div)
+        eventContainer.append(link)
+    
+    });
+}
+async function fetchEventNames() {
+    const response = await fetch('https://international-ashly-waffles-bedc2f70.koyeb.app/api/Event_Names');
+    const data = await response.json();
+    return data;
+}
+document.addEventListener("DOMContentLoaded", () => {
+    generateEvents();
+});
+
+// old Code
+/*
 var week1STD = 20.730805684966178;
 async function generateRankings() {
     var leagueTeams = [
@@ -294,3 +321,4 @@ function downloadCSV(filename, content) {
     a.click();
     // document.body.removeChild(a);
 }
+    */
