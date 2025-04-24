0c068bb296c5166e6732393e7f1273ebd3f93dc6
NateDCoder <rubyandtwixie@gmail.com>
2025-04-24 14:14:56 -0400

    Made it so it actually grabed data based off the year

--- Javscript Files/teams.js
+++ Javscript Files/teams.js
@@ -25,29 +25,32 @@ function sortTable(columnIndex) {
 
     // Append sorted rows
     tbody.innerHTML = "";
-    rows.forEach(row => tbody.appendChild(row));
+    rows.forEach((row) => tbody.appendChild(row));
 }
 async function fetchTeamList(year) {
-    const response = await fetch('https://international-ashly-waffles-bedc2f70.koyeb.app/api/Team_List');
+    const response = await fetch(`https://international-ashly-waffles-bedc2f70.koyeb.app/api/${year}/Team_List`);
     const data = await response.json();
     return data;
 }
 async function getTeamCount(year) {
-    const response = await fetch(`https://international-ashly-waffles-bedc2f70.koyeb.app/api/${year}/Total_Teams_Count`);
+    const response = await fetch(
+        `https://international-ashly-waffles-bedc2f70.koyeb.app/api/${year}/Total_Teams_Count`
+    );
     const teamCount = await response.json();
     return teamCount;
 }
 async function populateYears() {
     try {
-        const response = await fetch('https://international-ashly-waffles-bedc2f70.koyeb.app/api/Year_List');
+        const response = await fetch(
+            "https://international-ashly-waffles-bedc2f70.koyeb.app/api/Year_List"
+        );
         const years = await response.json();
         for (let year of years) {
             const option = document.createElement("option");
             option.value = year;
             option.text = year;
 
-            document.getElementById("years").appendChild(option)
-
+            document.getElementById("years").appendChild(option);
         }
     } catch (error) {
         console.error("Unable to fetch data:", error);
@@ -69,26 +72,25 @@ async function fetchJSONData(filePath) {
 async function generateRankings(year) {
     let teamListData = await fetchTeamList(year);
     var teamData = [];
-    console.log(teamListData)
     for (let team of teamListData) {
         teamData.push({
-            "teamNumber": team["teamNumber"],
-            "name": team["name"],
+            teamNumber: team["teamNumber"],
+            name: team["teamName"],
             "EPA Rank": team["EPA Rank"],
-            "unitlessEPA": Math.round(team["Unitless EPA"]),
-            "epa": Math.round(team["EPA"] * 10) / 10,
-            "autoEPA": Math.round(team["Auto EPA"] * 10) / 10,
-            "teleopEPA": Math.round(team["TeleOp EPA"] * 10) / 10,
-            "endgameEPA": Math.round(team["Endgame EPA"] * 10) / 10,
-            "nextEvent": "N/A",
-            "record": "N/A",
+            unitlessEPA: Math.round(team["unitlessEPA"]),
+            epa: Math.round(team["totalEPA"] * 10) / 10,
+            autoEPA: Math.round(team["autonEPA"] * 10) / 10,
+            teleopEPA: Math.round(team["teleOpEPA"] * 10) / 10,
+            endgameEPA: Math.round(team["endgameEPA"] * 10) / 10,
+            nextEvent: "N/A",
+            record: `${team["wins"]}-${team["loss"]}-${team["ties"]}`,
             "Auto EPA Rank": team["Auto EPA Rank"],
             "TeleOp EPA Rank": team["TeleOp EPA Rank"],
-            "Endgame EPA Rank": team["Endgame EPA Rank"]
-        })
+            "Endgame EPA Rank": team["Endgame EPA Rank"],
+        });
     }
 
-    return teamData
+    return teamData;
 }
 
 // Function to populate the table
@@ -97,8 +99,8 @@ async function populateTable(data, year) {
 
     // Clear existing rows (if any)
     tableBody.innerHTML = "";
-    const totalAmountOfTeams = await getTeamCount(year)
-    console.log(totalAmountOfTeams)
+    const totalAmountOfTeams = await getTeamCount(year);
+    console.log(totalAmountOfTeams);
     // Loop through the data and create rows
     for (let team of data) {
         const row = document.createElement("tr");
@@ -109,47 +111,46 @@ async function populateTable(data, year) {
                 const cell = document.createElement("td");
 
                 // Check if this is the second row
-                if (index === 1) {  // 0-based index, so 1 is the second row
+                if (index === 1) {
+                    // 0-based index, so 1 is the second row
                     const link = document.createElement("a");
-                    link.href = "./team htmls/" + team["number"] + ".html";  // Set the desired link
-                    link.textContent = value;  // Add the value as the link text
+                    link.href = "./team htmls/" + team["number"] + ".html"; // Set the desired link
+                    link.textContent = value; // Add the value as the link text
                     cell.appendChild(link);
                 } else if (4 <= index && index <= 7) {
-                    const div = document.createElement("div");  
+                    const div = document.createElement("div");
                     let rank;
                     switch (index) {
                         case 4:
-                            rank = "EPA Rank"
+                            rank = "EPA Rank";
                             break;
                         case 5:
-                            rank = "Auto EPA Rank"
+                            rank = "Auto EPA Rank";
                             break;
                         case 6:
-                            rank = "TeleOp EPA Rank"
+                            rank = "TeleOp EPA Rank";
                             break;
                         case 7:
-                            rank = "Endgame EPA Rank"
+                            rank = "Endgame EPA Rank";
                             break;
                     }
                     let percentile = 1 - team[rank] / totalAmountOfTeams;
 
-                    if (percentile < .25) {
-                        div.className = "red-style"
-                    } else if (percentile < .75) {
-                        div.className = "white-style"
-                    } else if (percentile < .9) {
-                        div.className = "light-green-style"
-                    } else if (percentile < .99) {
-                        div.className = "dark-green-style"
+                    if (percentile < 0.25) {
+                        div.className = "red-style";
+                    } else if (percentile < 0.75) {
+                        div.className = "white-style";
+                    } else if (percentile < 0.9) {
+                        div.className = "light-green-style";
+                    } else if (percentile < 0.99) {
+                        div.className = "dark-green-style";
                     } else {
-                        div.className = "blue-style"
-
+                        div.className = "blue-style";
                     }
-                    div.textContent = value
-                    cell.appendChild(div)
-                }
-                else {
-                    cell.textContent = value;  // For other rows, just set text content
+                    div.textContent = value;
+                    cell.appendChild(div);
+                } else {
+                    cell.textContent = value; // For other rows, just set text content
                 }
                 row.appendChild(cell);
             }
@@ -163,13 +164,15 @@ async function populateTable(data, year) {
 // Fetch JSON data and populate the table
 async function fetchAndPopulateTable() {
     try {
-        const selectedYear = document.getElementById("years").value
-        const data = await generateRankings(selectedYear); // Parse JSON
-        console.log(data)
+        const selectedYear = document.getElementById("years").value;
+        console.log(selectedYear);
+        const data = await generateRankings(selectedYear);
+        console.log(data);
         await populateTable(data, selectedYear); // Populate the table
     } catch (error) {
         console.error("Error fetching the JSON data:", error);
     }
+    sortTable(4);
 }
 
 // Call the function on page load
